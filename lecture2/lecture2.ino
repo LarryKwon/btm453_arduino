3eb077b8dc459c9ca24017d560887f5f9887a7b1
larrykwon <koallarry11@kaist.ac.kr>
2023-05-25 17:10:40 +0900

    control motor with switch

--- lecture2/lecture2.ino
+++ lecture2/lecture2.ino
@@ -1,18 +1,37 @@
 #include <Servo.h>
 
 Servo servo;
-
 const byte servo_signal = 4;
+
+const byte red_led = 6;
+const byte green_led = 5;
+const byte blue_led = 7;
+const byte joystick_sw = 3;
+
+int val_x;
+int pulse_width = 544;
+int delta_width;
 
 void setup(){
   servo.attach(servo_signal);
-
+  pinMode(blue_led, OUTPUT);
+  pinMode(green_led, OUTPUT);
+  pinMode(blue_led, OUTPUT);
+  pinMode(joystick_sw, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(joystick_sw), tact, CHANGE);
 }
 
 void loop(){
+  val_x = analogRead(A0);
+  // val_y = analogRead(A1);
 
-  servo.write(180);
-  delay(1000);
-  servo.write(0);
-  delay(1000);
+  delta_width = map(val_x, 0, 1023, -60, 60);
+  pulse_width += delta_width;
+  
+  servo.writeMicroseconds(pulse_width);
+  delay(10);
+}
+
+void tact(){
+  digitalWrite(blue_led, !digitalRead(joystick_sw));
 }
