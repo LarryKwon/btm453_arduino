cbc53c1cdd908efb37a2b78d7b875409f83ce74c
larrykwon <koallarry11@kaist.ac.kr>
2023-05-25 16:30:33 +0900

    with various tone

--- lecture2/lecture2.ino
+++ lecture2/lecture2.ino
@@ -1,5 +1,8 @@
 int pin_buzzer = 2;
 
+int Tones[7] = {262, 294, 330, 349, 392, 440, 494};
+int Tones_Num;
+
 void setup() {
   // put your setup code here, to run once:
   pinMode(pin_buzzer, OUTPUT);
@@ -8,11 +11,13 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
 
-  // output sound
-  tone(pin_buzzer,262);
-  delay(1000);
+  for(Tones_Num = 0; Tones_Num < 7; Tones_Num++){
+    // output sound
+    tone(pin_buzzer,Tones[Tones_Num]);
+    delay(500);
 
-  // stop sound
-  noTone(pin_buzzer);
-  delay(1000);
+    // stop sound
+    noTone(pin_buzzer);
+    delay(500);    
+  }
 }
